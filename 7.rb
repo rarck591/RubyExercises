3e5f11a1cf72e8e2a44f5f49b137ea6386ed9b73
Rey Arceno <reyarceno@gmail.com>
2013-09-03 16:32:19 -0500

    Create 7.rb

---
+++ 7.rb
@@ -0,0 +1,6 @@
+# 7. What's the major difference between an Array and a Hash?
+
+# Hashes contain keys that cannot be repeated and are paired with values that can pretty much be anything.  
+In arrays, you can repeat keys, but the values are always integers.  
+Arrays also maintain a sense of order.  
+Hashes do not.
